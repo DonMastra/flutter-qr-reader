e57699f85255419d24107fd9416cfbbcea10636c
Andres Mastracchio <mastracchioandres@gmail.com>
2021-06-21 23:05:58 -0300

    initial commit, qr-parcial-reading complete

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/maps_page.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
+
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete_forever),
+          )
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Get selectedMenuOpt form provider
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    // Change to show current page
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return MapsPage();
+
+      case 1:
+        return AdressesPage();
+
+      default:
+        return MapsPage();
+    }
+  }
+}
